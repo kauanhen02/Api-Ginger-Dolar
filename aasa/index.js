15b6a400fe6894e5e590c645cce1df076663f9a6
kauanhen02 <kauansjx31@gmail.com>
2024-02-26 15:36:59 -0300

    first commit

---
+++ aasa/index.js
@@ -0,0 +1,31 @@
+const axios = require("axios")
+
+const url =
+  "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json"
+
+async function obterCotacaoDolarPTAXVenda() {
+  try {
+    const response = await axios.get(url)
+    const dados = response.data
+    const dataAtual = new Date().toLocaleDateString()
+
+    let valorEncontrado = null
+
+    for (let i = 0; i < dados.length; i++) {
+      if (dataAtual === dados[i].data) {
+        valorEncontrado = dados[i].valor
+        break
+      }
+    }
+
+    if (valorEncontrado !== null) {
+      console.log("Cotação do dólar PTAX de venda:", valorEncontrado)
+    } else {
+      console.log("Cotação não encontrada para a data atual.")
+    }
+  } catch (error) {
+    console.error("Erro ao obter a cotação:", error)
+  }
+}
+
+obterCotacaoDolarPTAXVenda()

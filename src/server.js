472b08eda7605a1d9c6ea828571b88819fffd1f7
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2024-03-01 15:28:30 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const axios = require("axios");
 const fs = require("fs");
-const path = require("path"); // Importe o módulo 'path'
+const path = require("path"); 
 
 const app = express();
 const PORT = process.env.PORT || 3077;
 const url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1/dados?formato=json";
 const jsonFilePath = path.join(__dirname, "cotacao_dolar.json");
 
-const cotacaodolar = require("./cotacao_dolar.json");
-
 app.get("/cotacaodolar", (req, res) =>{
   try {
-    // Lê o conteúdo do arquivo JSON a cada solicitação
     const cotacaodolar = JSON.parse(fs.readFileSync(jsonFilePath));
     res.json(cotacaodolar);
   } catch (error) {
@@ -21,10 +18,8 @@ app.get("/cotacaodolar", (req, res) =>{
   }
 })
 
-// Função para verificar e inicializar o arquivo JSON se estiver vazio ou malformado
 function inicializarArquivoJSON() {
   try {
-    // Verifica se o arquivo existe, senão cria um novo
     if (!fs.existsSync(jsonFilePath)) {
       fs.writeFileSync(jsonFilePath, "[]");
     }
@@ -33,7 +28,6 @@ function inicializarArquivoJSON() {
   }
 }
 
-// Inicializa o arquivo JSON quando o servidor é iniciado
 inicializarArquivoJSON();
 
 async function obterCotacaoDolarPTAXVenda() {
@@ -63,7 +57,6 @@ async function obterCotacaoDolarPTAXVenda() {
       const jsonResultado = JSON.stringify(resultado);
       console.log(jsonResultado);
 
-      // Salvar os dados em um arquivo JSON
       const dadosSalvos = JSON.parse(fs.readFileSync(jsonFilePath));
       dadosSalvos.push(resultado);
       fs.writeFileSync(jsonFilePath, JSON.stringify(dadosSalvos, null, 2));
@@ -75,10 +68,8 @@ async function obterCotacaoDolarPTAXVenda() {
   }
 }
 
-// Execute a função a cada 10 segundos
-setInterval(obterCotacaoDolarPTAXVenda, 5000); // 10 segundos em milissegundos
+setInterval(obterCotacaoDolarPTAXVenda, 5000);
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
